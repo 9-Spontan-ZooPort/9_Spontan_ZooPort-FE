b2f73f7547d20652a4d8e11b44f1fe1bfd0b0603
ahargunyllib <billy.bpm03@gmail.com>
2024-04-27 13:21:39 +0700

    feat: adding regsiter feat

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -24,3 +24,35 @@ export async function login(email: string, password: string) {
 		return { error: message };
 	}
 }
+
+export async function register(
+	name: string,
+	email: string,
+	password: string,
+	role: "admin" | "zookeeper",
+) {
+	try {
+		const response = await fetch("http://localhost:8080/v1/auth/register", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Superadmin: "superadmin",
+			},
+			body: JSON.stringify({ name, email, password, role }),
+			mode: "no-cors",
+		});
+
+		console.log(response);
+
+		if (!response.ok) {
+			console.log("Error: ", response.statusText);
+			throw new Error("User already registered");
+		}
+
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		const message = (error as Error).message;
+		return { error: message };
+	}
+}
